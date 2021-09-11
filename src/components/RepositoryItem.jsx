f0a1f3b77f444ec19362efd583b2da21713b7da0
Jonathan Martins <jonathan.rws@gmail.com>
2021-09-10 21:54:25 -0300

    Meu primeiro commit

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,7 @@
+export function RepositoryItem(props){
+ return <li>
+  <strong>{props.repository.full_name}</strong>
+  <p>{props.repository.description}</p>
+  <a href={props.repository.html_url}>Acessar repositório</a>
+</li>
+}
